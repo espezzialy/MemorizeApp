2f790bd8926cf8337a7e70aefb2946a68c947a94
Espezzialy Souza <espezzialy@ciandt.com>
2021-03-19 00:02:12 -0300

    Initial Commit

---
+++ MemorizeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizeApp
+//
+//  Created by Espezzialy Raphael Oliveira Souza on 19/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
