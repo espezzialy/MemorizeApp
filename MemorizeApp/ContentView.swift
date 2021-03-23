c22c3d9bf231e1dfa2745e1446b06aed0b46aec7
Espezzialy Souza <espezzialy@ciandt.com>
2021-03-22 22:10:16 -0300

    Finish first assinagment

--- MemorizeApp/ContentView.swift
+++ MemorizeApp/ContentView.swift
@@ -8,45 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
+    
+    
     var body: some View {
         return HStack {
-            ForEach(0..<4) { index in
-                CardView(isFaceUp: true)
+            ForEach(viewModel.cards.shuffled()) { card in
+                CardView(card: card).onTapGesture {
+                    viewModel.choose(card: card)
+                }
             }
         }
-        .font(Font.largeTitle)
+        .font(fontSize(numberOfCards: viewModel.cards.count))
         .padding()
         .foregroundColor(Color.orange)
     }
 }
 
 struct CardView: View {
-    var isFaceUp: Bool
+    var card: MemoryGame<String>.Card
     
     var body: some View {
         ZStack {
-            if isFaceUp {
+            if card.isFaceUp {
             RoundedRectangle(cornerRadius: 10.0 ).fill(Color.white)
             RoundedRectangle(cornerRadius: 10.0 ).stroke(lineWidth: 3)
-            Text("👻")
+                Text(card.content)
             } else {
                 RoundedRectangle(cornerRadius: 10.0 ).fill()
             }
             
         }
+        .aspectRatio(CGSize(width: 2, height: 3), contentMode: .fit)
     }
+
 }
-
-
-
-
-
-
-
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: EmojiMemoryGame())
+    }
+}
+
+func fontSize(numberOfCards: Int) -> Font {
+    if numberOfCards == 10 {
+        return Font.title3
+    } else {
+        return Font.largeTitle
     }
 }
--- MemorizeApp/EmojiMemoryGame.swift
+++ MemorizeApp/EmojiMemoryGame.swift
@@ -5,4 +5,26 @@
 //  Created by Espezzialy Raphael Oliveira Souza on 19/03/21.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() ->  MemoryGame<String> {
+        let emojis: Array<String> = ["👻","🎃", "🕷", "👺", "💀"]
+        let randomNumber = Int.random(in: 2...5)
+        return MemoryGame<String>(numberOfPairsOfCards: randomNumber) { pairIndex in
+            return emojis[pairIndex]
+        }
+    }
+//    MARK: - Access to the model
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+//    MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
